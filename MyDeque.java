7f2a912fa8d9a18c506869e837084766f8ca1371
Jude-Rizzo <jprizzo2020@gmail.com>
2019-03-25 11:46:54 -0400

    leon

--- MyDeque.java
+++ MyDeque.java
@@ -98,9 +98,29 @@ public class MyDeque<E>{
 
       return;
     }
+
     //for new cases, copy data into dataCopy
-    //then change up data do resize import junit.framework.TestCase;
-    
+    dataCopy = Arrays.copy(data);
+    //Then clear data();
+    data = new int[size *2];
+    size = size * 2;
+    //NOW see if end > start and such
+    if(end > start){
+      for(int i = start; i <= end; i++){
+        data[i - start] = dataCopy[i];
+      }
+    } else {
+      for(int i = start; i < dataCopy.length; i++){
+          data[i - start] = dataCopy[i];
+      }
+      for(int i = 0; i <= end; i++){
+        data[i + (data.length - start)] = dataCopy[i];
+      }
+    }
+    start = 0;
+    end = dataCopy.length - 1;
+    //now redo start and end
+
     //copy all the starts into the beginning, then make the array in order again cuz y not
 
   }
