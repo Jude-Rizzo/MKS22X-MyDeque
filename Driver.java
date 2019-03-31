e29ee7c1c2f2b8a2aeb2e6d8c403280ab3f604d1
Jude Rizzo <jrizzo00@stuy.edu>
2019-03-31 11:08:24 -0400

    still fixing

---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver{
+
+  public static void main(String[] ans){
+    MyDeque tester = new MyDeque();
+    System.out.println(tester.toString());
+    MyDeque test = new MyDeque(10);
+    System.out.println(tester.toString());
+  }
+}
--- MyDeque.java
+++ MyDeque.java
@@ -1,3 +1,6 @@
+import java.util.*;
+
+
 public class MyDeque<E>{
   private E[] data;
   private int size, start, end;
@@ -22,4 +25,70 @@ public class MyDeque<E>{
    start = 0;
    end = 0;
  }
+
+ public int size(){
+   return size;
+ }
+
+ public String toString(){
+   if(size == 0){
+     return "{}";
+   }
+   String ans = "{";
+   if(start < end){
+     for(int i = start; i <= end; i++){
+
+       if(data[i] != null) ans += data[i] + " ";
+
+     } return ans + " }";
+   }
+
+   if(start > end){
+     for(int i = start; i < data.length; i++){
+       if(data[i] != null) ans += data[i] + " ";
+     }
+     for(int i = 0; i <= end; i++){
+       if(data[i] != null) ans += data[i] + " ";
+     }
+   }
+   return ans + "}";
+
+ }
+
+ private void resize(){
+   E[] dataCopy = Arrays.copyOf(data, data.length);
+   @SuppressWarnings("unchecked")
+   E[] data = (E[]) new Object[size * 2 + 1];
+   size = size * 2 + 1;
+   int index = 0;
+   if(start > end){
+     for(int i = start; i < data.length; i++){
+       //starts at 0;
+       data[i - start] = dataCopy[i];
+       index +=1;
+     }
+     for(int i = 0; i <= end; i++){
+       data[i + index] = dataCopy[i];
+     }
+
+     start = 0;
+     end = end + index;
+     //copy to end ad the from start
+   }
+
+   if(end >= start){
+     for(int i = start; i <= end; i++){
+       data[i - start] = dataCopy[i];
+     }
+
+     start = 0;
+     end = end - start;
+   }
+
+ }
+
+
+
+
+
 }
